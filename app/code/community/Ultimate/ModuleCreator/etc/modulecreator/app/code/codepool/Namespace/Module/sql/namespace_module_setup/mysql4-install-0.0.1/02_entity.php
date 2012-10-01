9678ac2370262d0bb59e948966236aea032f55cb
Marius Strajeru <tzyganu@gmail.com>
2012-10-01 20:02:27 +0300

    fix missign files

---
+++ app/code/community/Ultimate/ModuleCreator/etc/modulecreator/app/code/codepool/Namespace/Module/sql/namespace_module_setup/mysql4-install-0.0.1/02_entity.php
@@ -0,0 +1,9 @@
+$this->run("
+	CREATE TABLE IF NOT EXISTS `{$this->getTable('{{module}}/{{entity}}')}` (
+		`entity_id` int(10) unsigned NOT NULL auto_increment,
+{{attributeSql}}
+		`created_at` datetime NULL,
+		`updated_at` datetime NULL,
+	PRIMARY KEY (`entity_id`)) 
+	ENGINE=InnoDB DEFAULT CHARSET=utf8;
+"); 
